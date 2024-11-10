2fbfcaa6aee6ddeb0d4bd66b1a3b2a2981fa0f2e
Gnaneshwari Gunasekaran <gnaneshwarigunasekaran@gmail.com>
2024-11-10 17:44:49 +0530

    Add production build files to docs

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-JKD7KGGT.mjs";import"./chunk-DX5STH77.mjs";import"./chunk-XMXHVHWN.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-3F3AV3L5.mjs";import"./chunk-DX5STH77.mjs";import"./chunk-XMXHVHWN.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
