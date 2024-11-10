c49e91ea66999f3d85be574fcedaf58cc8bfab96
Gnaneshwari Gunasekaran <gnaneshwarigunasekaran@gmail.com>
2024-11-10 17:33:24 +0530

    build changes

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-WWXNOXRW.mjs";import"./chunk-DX5STH77.mjs";import"./chunk-XMXHVHWN.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-JKD7KGGT.mjs";import"./chunk-DX5STH77.mjs";import"./chunk-XMXHVHWN.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
